ea19ae74bdfd70002173176f2bf09f837381cfdb
MchBr02 <42812980+MchBr02@users.noreply.github.com>
2025-03-08 17:03:30 +0100

    bug fix

--- start.sh
+++ start.sh
@@ -33,7 +33,7 @@ install_mongodb() {
             sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg --dearmor
 
         # Add MongoDB repository
-        log "deb [ signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/8.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+        echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/8.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
 
         # Install MongoDB
         sudo apt-get update
